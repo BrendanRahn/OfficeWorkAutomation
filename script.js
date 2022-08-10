332e6822bdcc4ee7046ca12fab4cac677541c31f
Brendan Rahn <brendanrahn01@gmail.com>
2022-08-10 14:58:54 -0500

    commiting current progress

--- script.js
+++ script.js
@@ -394,7 +394,7 @@ async function enterBox24_1(driver) {
     await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DOS_COMMENTS0"))
         .sendKeys(johnDoeJson.box24[1].note);
 
-    await driver.findElement(By.id("#ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_START_TIME0"))
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_START_TIME0"))
         .sendKeys(johnDoeJson.box24[1].anestStart);
 
     await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_STOP_TIME0"))
@@ -402,6 +402,274 @@ async function enterBox24_1(driver) {
 
     await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DRUG_QUALIFIER0"))
         .sendKeys(johnDoeJson.box24[1].NDCQual);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCCODE0"))
+        .sendKeys(johnDoeJson.box24[1].NDCCode);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCCHARGE0"))
+        .sendKeys(johnDoeJson.box24[1].NDCUPrice);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCUNITS0"))
+        .sendKeys(johnDoeJson.box24[1].NDCQty);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCMEASURE0"))
+        .sendKeys(johnDoeJson.box24[1].NDCQtyQual);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_RENDERING_PHYSICIAN_QUALIFIER0"))
+        .sendKeys(johnDoeJson.box24[1].IDQual);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_RENDERING_PHYSICIAN_ID0"))
+        .sendKeys(johnDoeJson.box24[1].renderingProviderId)
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_FM_DATE_OF_SVC_MONTH0"))
+        .sendKeys(johnDoeJson.box24[1].serviceDateFrom);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_TO_DATE_OF_SVC_MONTH0"))
+        .sendKeys(johnDoeJson.box24[1].serviceDateTo);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_PLACE_OF_SVC0"))
+        .sendKeys(johnDoeJson.box24[1].placeOfService);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_EMG0"))
+        .sendKeys(johnDoeJson.box24[1].EMG);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_CPT_CODE0"))
+        .sendKeys(johnDoeJson.box24[1].CPT_HCPCs);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_A0"))
+        .sendKeys(johnDoeJson.box24[1].modifierA);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_B0"))
+        .sendKeys(johnDoeJson.box24[1].modifierB);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_C0"))
+        .sendKeys(johnDoeJson.box24[1].modifierC);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_D0"))
+        .sendKeys(johnDoeJson.box24[1].modifierD);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DOS_DIAG_CODE0"))
+        .sendKeys(johnDoeJson.box24[1].diagnosisPointer);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DOS_CHRG0"))
+        .sendKeys(johnDoeJson.box24[1].charges);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_UNITS0"))
+        .sendKeys(johnDoeJson.box24[1].daysOrUnits);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_EPSDT_FAMILY_PLAN0"))
+        .sendKeys(johnDoeJson.box24[1].EPSDTFamilyPlan);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_RENDERING_PHYSICIAN_NPI0"))
+        .sendKeys(johnDoeJson.box24[1].NPI);
+}
+
+async function enterBox24_2(driver) {
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DOS_COMMENTS1"))
+        .sendKeys(johnDoeJson.box24[2].note);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_START_TIME1"))
+        .sendKeys(johnDoeJson.box24[2].anestStart);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_STOP_TIME1"))
+        .sendKeys(johnDoeJson.box24[2].anestEnd);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DRUG_QUALIFIER1"))
+        .sendKeys(johnDoeJson.box24[2].NDCQual);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCCODE1"))
+        .sendKeys(johnDoeJson.box24[2].NDCCode);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCCHARGE1"))
+        .sendKeys(johnDoeJson.box24[2].NDCUPrice);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCUNITS1"))
+        .sendKeys(johnDoeJson.box24[2].NDCQty);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCMEASURE1"))
+        .sendKeys(johnDoeJson.box24[2].NDCQtyQual);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_RENDERING_PHYSICIAN_QUALIFIER1"))
+        .sendKeys(johnDoeJson.box24[2].IDQual);
+
+    //read-only box
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_RENDERING_PHYSICIAN_ID1"))
+        .sendKeys(johnDoeJson.box24[2].renderingProviderId);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_FM_DATE_OF_SVC_MONTH1"))
+        .sendKeys(johnDoeJson.box24[2].serviceDateFrom);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_TO_DATE_OF_SVC_MONTH1"))    
+        .sendKeys(johnDoeJson.box24[2].serviceDateTo);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_PLACE_OF_SVC1"))    
+        .sendKeys(johnDoeJson.box24[2].placeOfService);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_EMG1"))
+        .sendKeys(johnDoeJson.box24[2].EMG);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_CPT_CODE1"))
+        .sendKeys(johnDoeJson.box24[2].CPT_HCPCs);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_A1"))
+        .sendKeys(johnDoeJson.box24[2].modifierA);    
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_B1"))
+        .sendKeys(johnDoeJson.box24[2].modifierB);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_C1"))
+        .sendKeys(johnDoeJson.box24[2].modifierC);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_D1"))
+        .sendKeys(johnDoeJson.box24[2].modifierD);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DOS_DIAG_CODE1"))
+        .sendKeys(johnDoeJson.box24[2].diagnosisPointer);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DOS_CHRG1"))
+        .sendKeys(johnDoeJson.box24[2].charges);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_UNITS1"))
+        .sendKeys(johnDoeJson.box24[2].daysOrUnits);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_EPSDT_FAMILY_PLAN1"))
+        .sendKeys(johnDoeJson.box24[2].EPSDTFamilyPlan);
+
+    //read-only box (idk why)
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_RENDERING_PHYSICIAN_NPI1"))
+        .sendKeys(johnDoeJson.box24[2].NPI);
+}
+
+async function enterBox24_3(driver) {
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DOS_COMMENTS2"))
+        .sendKeys(johnDoeJson.box24[3].note);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_START_TIME2"))
+        .sendKeys(johnDoeJson.box24[3].anestStart);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_STOP_TIME2"))
+        .sendKeys(johnDoeJson.box24[3].anestEnd);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DRUG_QUALIFIER2"))
+        .sendKeys(johnDoeJson.box24[3].NDCQual);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCCODE2"))
+        .sendKeys(johnDoeJson.box24[3].NDCCode);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCCHARGE2"))
+        .sendKeys(johnDoeJson.box24[3].NDCUPrice);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCUNITS2"))
+        .sendKeys(johnDoeJson.box24[3].NDCQty);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_NDCMEASURE2"))
+        .sendKeys(johnDoeJson.box24[3].NDCQtyQual);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_RENDERING_PHYSICIAN_QUALIFIER2"))
+        .sendKeys(johnDoeJson.box24[3].IDQual);
+
+    //read-only box
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_RENDERING_PHYSICIAN_ID2"))
+        .sendKeys(johnDoeJson.box24[3].renderingProviderId);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_FM_DATE_OF_SVC_MONTH2"))
+        .sendKeys(johnDoeJson.box24[3].serviceDateFrom);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_TO_DATE_OF_SVC_MONTH2"))
+        .sendKeys(johnDoeJson.box24[3].serviceDateTo);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_PLACE_OF_SVC2"))
+        .sendKeys(johnDoeJson.box24[3].placeOfService);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_EMG2"))
+        .sendKeys(johnDoeJson.box24[3].EMG);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_CPT_CODE2"))
+        .sendKeys(johnDoeJson.box24[3].CPT_HCPCs);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_A2"))
+        .sendKeys(johnDoeJson.box24[3].modifierA);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_B2"))
+        .sendKeys(johnDoeJson.box24[3].modifierB);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_C2"))
+        .sendKeys(johnDoeJson.box24[3].modifierC);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_MODIFIER_D2"))
+        .sendKeys(johnDoeJson.box24[3].modifierD);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DOS_DIAG_CODE2"))
+        .sendKeys(johnDoeJson.box24[3].diagnosisPointer);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_DOS_CHRG2"))
+        .sendKeys(johnDoeJson.box24[3].charges);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_UNITS2"))
+        .sendKeys(johnDoeJson.box24[3].daysOrUnits);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_EPSDT_FAMILY_PLAN2"))
+        .sendKeys(johnDoeJson.box24[3].EPSDTFamilyPlan);
+
+    //read-only box
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ucHCFALineItem_ucClaimLineItem_RENDERING_PHYSICIAN_NPI2"))
+        .sendKeys(johnDoeJson.box24[3].NPI);
+}
+
+async function enterBox25(driver) {
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_HCFA_TAX_ID"))
+        .sendKeys(johnDoeJson.box25.federalTaxIdNumber);
+
+    if(johnDoeJson.box25.TaxIdType == "SSN") {
+        await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_HCFA_SSN_EIN1")).click();
+    }   
+        else {
+            await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_HCFA_SSN_EIN2")).click();
+        }
+}
+
+async function enterBox26(driver) {
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_PATIENT_ACCT_NUM"))
+        .sendKeys(johnDoeJson.box26.paitentsAccountNumber);
+}
+
+async function enterBox27(driver) {
+    if (johnDoeJson.box27.acceptAssignment == "YES") {
+        await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ACCEPT_SIGNATURE1")).click();
+    }
+        else {
+            await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_ACCEPT_SIGNATURE2")).click();
+        }
+}
+
+async function enterBox28(driver) {
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_TOTAL_CHARGE"))
+        .sendKeys(johnDoeJson.box28.toaalCharge);
+}
+
+async function enterBox29(driver) {
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_AMOUNT_PAID"))
+        .sendKeys(johnDoeJson.box29.amountPaid);
+}
+
+async function enterBox31(driver) {
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_DateInitialTreatment_Month"))
+        .sendKeys(johnDoeJson.box31.initialTreatmentDate);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_DateLastSeen_Month"))
+        .sendKeys(johnDoeJson.box31.latestVisitDate);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_SUPERVISING_PHYSICIAN"))
+        .sendKeys(johnDoeJson.box31.supervisingPhysician);
+
+    await driver.findElement(By.id("ctl00_phFolderContent_ucHCFA_SUPERVISING_PHYSICIAN_NPI"))
+        .sendKeys(johnDoeJson.box31.supervisingPhysicianId);
+
+    await driver.findElement(By.id(""))
+
+    await driver.findElement(By.id(""))
+
+    await driver.findElement(By.id(""))
 
     await driver.findElement(By.id(""))
 
@@ -456,6 +724,40 @@ async function fillOutClaim(driver) {
     enterBox17(driver);
 
     enterBox18(driver);
+
+    enterBox19(driver);
+
+    enterBox20(driver);
+
+    enterBox21(driver);
+
+    enterBox22(driver);
+
+    enterBox23(driver);
+
+    //box 24 sub-boxes are labeled in html beginning at 0, but displayed as beginning at 1 >:(
+    enterBox24_1(driver);
+
+    enterBox24_2(driver);
+
+    enterBox24_3(driver);
+
+    enterBox25(driver);
+
+    enterBox26(driver);
+
+    enterBox27(driver);
+
+    enterBox28(driver);
+
+    enterBox29(driver);
+
+    //box30 is read-only
+
+
+
+
+
 
 }
 
